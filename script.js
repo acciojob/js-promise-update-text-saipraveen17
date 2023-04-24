f0d42bcb947fe590f6a5df16487087447bdcdbb0
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-24 16:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
 function sayHello() {
 	return new Promise((resolve,reject)=>{
-		setTimeout(resolve('Hello, world!'),1000);
+		setTimeout(()=>{
+			resolve('Hello, world!');
+		},1000);
 	});
 }
 sayHello().then((result)=>{
