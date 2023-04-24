982ad54b0ce787597a31b3ac04bdd14160711c57
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-24 15:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+Promise promise = new Promise(()=>{
+	setTimeout(resolve(),1000);
+});
+promise.then(()=>{
+	const ele = document.getElementById('output');
+	ele.innerText = 'Hello, world!';
+});
